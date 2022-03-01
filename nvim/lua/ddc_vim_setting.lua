57822add1a2758093a34f867040a4262014efbe7
msattova <55355782+msattova@users.noreply.github.com>
2022-03-02 00:45:34 +0900

    Update ddc_vim_setting.lua

--- nvim/lua/ddc_vim_setting.lua
+++ nvim/lua/ddc_vim_setting.lua
@@ -2,13 +2,25 @@
 vim.cmd[[
 call ddc#custom#patch_global('sources', [
     \ 'around',
-    \ 'file'
+    \ 'file',
+    \ 'nvim-lsp',
+    \ 'skkeleton'
     \ ])
 call ddc#custom#patch_global('sourceOptions', {
     \ '_': {
     \    'matchers': ['matcher_head'],
     \    'sorters' : ['sorter_rank'],
     \    'converters' : ['converter_remove_overlap'],
+    \ },
+    \ 'skkeleton': {
+    \    'mark': 'skkeleton',
+    \    'matchers': ['skkeleton'],
+    \    'sorters': [],
+    \    'minAutoCompleteLength': 2,
+    \ },
+    \ 'nvim-lsp': {
+    \    'mark': 'lsp',
+    \    'forceCompletionPattern': '\.\w*|:\w*|->\w*'
     \ },
     \ 'file': {
     \   'mark': 'file',
@@ -17,49 +29,13 @@ call ddc#custom#patch_global('sourceOptions', {
     \ }
     \ })
 call ddc#custom#patch_global('completionMenu', 'pum.vim')
-
-call ddc#enable()
 inoremap <silent><expr> <TAB>
-    \ pum#visible() ? '<Cmd>call pum#map#insert_relative(+1)<CR>' :
+    \ pum#visible() ? '<Cmd> call pum#map#insert_relative(+1)<CR>' :
     \ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
     \ '<TAB>' : ddc#manual_complete()
 inoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
-inoremap <C-n>  <Cmd>call pum#map#select_relative(+1)<CR>
-inoremap <C-p>  <Cmd>call pum#map#select_relative(-1)<CR>
-inoremap <C-y>  <Cmd>call pum#map#confirm()<CR>
-inoremap <C-e>  <Cmd>call pum#map#cancel()<CR>
-inoremap <PageDown>  <Cmd>call pum#map#select_relative(+1)<CR>
-inoremap <PageUp>  <Cmd>call pum#map#select_relative(-1)<CR>
-
-call ddc#custom#patch_global('autoCompleteEvents', [
-    \ 'InsertEnter', 'TextChangedI', 'TextChangedP',
-    \ 'CmdlineEnter', 'CmdlineChanged',
-    \ ])
-
-nnoremap :       <Cmd>call CommandlinePre()<CR>:
-
-function! CommandlinePre() abort
-  " Note: It disables default command line completion!
-  cnoremap <expr> <Tab>
-  \ pum#visible() ? '<Cmd>call pum#map#insert_relative(+1)<CR>' :
-  \ ddc#manual_complete()
-  cnoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
-  cnoremap <C-y>   <Cmd>call pum#map#confirm()<CR>
-  cnoremap <C-e>   <Cmd>call pum#map#cancel()<CR>
-
-  " Overwrite sources
-  let s:prev_buffer_config = ddc#custom#get_buffer()
-  call ddc#custom#patch_buffer('sources',
-          \ ['cmdline', 'cmdline-history', 'around'])
-
-  autocmd User DDCCmdlineLeave ++once call CommandlinePost()
-
-  " Enable command line completion
-  call ddc#enable_cmdline_completion()
-  call ddc#enable()
-endfunction
-function! CommandlinePost() abort
-
-endfunction
-
+inoremap <C-n> <Cmd>call pum#map#select_relative(+1)<CR>
+inoremap <C-p> <Cmd>call pum#map#select_relative(-1)<CR>
+inoremap <C-y> <Cmd>call pum#map#confirm()<CR>
+inoremap <C-e> <Cmd>call pum#map#cancel()<CR>
 ]]
